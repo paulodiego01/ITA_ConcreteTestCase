b438e67bed132e50f7eecc1bdbd1c39490c6ba07
paulodiego01 <paulodiego1@gmail.com>
2017-08-28 11:01:17 -0300

    Update compareRecDataWithOutput.m

--- ConcreteTestCase_/compareRecDataWithOutput.m
+++ ConcreteTestCase_/compareRecDataWithOutput.m
@@ -9,17 +9,17 @@ contadorTop = 1;
 count = 1;
 
 for t = 1:size(tests,2) % caminha pelos testes
-    for e = 1:size(tests(t).expression,2) % caminha em cada express„o do teste
+    for e = 1:size(tests(t).expression,2) % caminha em cada express√£o do teste
         
         recDataCounter = 1;
 
-        for rd = 1:size(tests(t).expression{e}.recdata,2) %caminho em cada recdata de uma express„o
+        for rd = 1:size(tests(t).expression{e}.recdata,2) %caminho em cada recdata de uma express√£o
             
 
             
             output(contadorTop).testNumber = tests(t).id;  % salva o id do teste
             output(contadorTop).ExprNumber = e;
-            output(contadorTop).ExprText = tests(t).expression{e}.text; % salva a express„o do teste
+            output(contadorTop).ExprText = tests(t).expression{e}.text; % salva a express√£o do teste
             output(contadorTop).sentTC = tests(t).expression{e}.senddata; % salva o telecomando enviado pelo teste
              
             output(contadorTop).parameterName = tests(t).expression{e}.recdata{rd}.parsed.name;
@@ -46,44 +46,6 @@ for t = 1:size(tests,2) % caminha pelos testes
          count = count + 1;
     end
 end
-
-
-
-
-% 
-% count = 1;
-% 
-% for t = 1:size(tests,1) % caminha pelos testes
-%     for e = 1:size(tests(t).expression,2) % caminha em cada express„o do teste
-% 
-%         output(count).testNumber = tests(t).id;  % salva o id do teste
-%         output(count).expression = tests(t).expression{e}.text; % salva a express„o do teste
-%         output(count).sentTC = tests(t).expression{e}.senddata; % salva o telecomando enviado pelo teste
-%         
-%         recDataCounter = 1;
-% 
-%         for rd = 1:size(tests(t).expression{e}.recdata,2) %caminho em cada recdata de uma express„o
-%              
-%             output(count).paramValue(recDataCounter).name = tests(t).expression{e}.recdata{rd}.parsed.name;
-%             nameToSearch = output(count).paramValue(recDataCounter).name;
-%             output(count).paramValue(recDataCounter).expectedValue = tests(t).expression{e}.recdata{rd}.parsed.parameter;
-%             exp = output(count).paramValue(recDataCounter).expectedValue;
-%             output(count).paramValue(recDataCounter).simulatedValue = getParameterActualState(nameToSearch,outputLog(count).outputs);
-%             sim = output(count).paramValue(recDataCounter).simulatedValue;
-%             
-%             if strcmp(exp,sim) == 1
-%                  output(count).paramValue(recDataCounter).comparation = 'OK';
-%             else
-%                  output(count).paramValue(recDataCounter).comparation = 'NOT OK';               
-%             end
-%             
-%             recDataCounter = recDataCounter + 1;
-%          end
-%          
-%          count = count + 1;
-%     end
-% end
-
 
 
 end
